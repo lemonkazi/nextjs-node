3e25aba37ad01d07cc153e37255a506b8c99a5dd
lemonkazi <lemonpstu09@gmail.com>
2025-08-03 17:51:08 +0600

    first commit

--- client/next.config.js
+++ client/next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    styledComponents: true,
+  compiler: {
+    styledComponents: true
   },
   webpack: (config) => {
     config.resolve.alias['@'] = __dirname;
